17b8ea8bf3de22d8bd61326349a74499b821c4ec
Arjun  Dass <ad.sam007@gmail.com>
2017-04-04 00:48:26 -0500

    object literals

---
+++ Node Practice/objectLiteral.js
@@ -0,0 +1,8 @@
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    greet: function(){
+        console.log("Hello " + this.firstname + ' : '    + this.lastname);
+    }
+};
+person.greet();

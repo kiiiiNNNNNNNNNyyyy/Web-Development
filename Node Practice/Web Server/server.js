0574906fd499f3976bc54527479de743451ff6a9
Arjun  Dass <ad.sam007@gmail.com>
2017-04-13 11:59:31 -0500

    Added heroku

--- Node Practice/Web Server/server.js
+++ Node Practice/Web Server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();
 
 //Middleware
 hbs.registerPartials(__dirname + '/views/partials');   
 app.set('view engine', 'hbs');  //telling the express which handlebars we are gonna use.
+app.use((req, res, next) => {   //if you won't call next(), the paeg will keep loading, waiting for route. Express middleware
+     var now = new Date().toString();
+     var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append!!');
+        }
+    });
+     next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
@@ -28,6 +47,6 @@ app.get('*', (req, res) => {
     res.send("<h1>ERROR 404</h1>"); 1
 });
 
-app.listen(3000, () => {
-    console.log("Server is ready at port 3000");
+app.listen(port, () => {
+    console.log("Server is ready at port: " + port);
 });
